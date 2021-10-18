769ed6939515d1b6b738a2e0ada6227d114ee11c
Igor Zubkov <igor.zubkov@gmail.com>
2021-10-18 18:25:28 +0300

    Mount engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # Almost every application defines a route for the root path ("/") at the top of this file.
-  # root "articles#index"
+  mount RailsPgExtrasWeb::Engine, at: "/"
 end
